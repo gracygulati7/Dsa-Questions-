88a959b7b7120dcc778c342e3d7f068ba4c67740
Gracy Gulati <124993594+gracygulati7@users.noreply.github.com>
2025-04-29 15:45:06 +0530

    Array Q6

---
+++ Arrays/Easy/Move Zeroes.java
@@ -0,0 +1,16 @@
+// https://leetcode.com/problems/move-zeroes/description/
+
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int n=nums.length;
+        int left=0;
+        for(int right=0;right<n;right++){
+            if(nums[right]!=0){
+                int temp=nums[left];
+                nums[left]=nums[right];
+                nums[right]=temp;
+                left++;
+            }
+        }
+    }
+}
