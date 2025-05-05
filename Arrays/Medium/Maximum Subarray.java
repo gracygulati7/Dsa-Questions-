43893df28588daa3facb939f223ba1aae0a2e5f3
Gracy Gulati <124993594+gracygulati7@users.noreply.github.com>
2025-05-05 12:12:45 +0530

    Array Q4

---
+++ Arrays/Medium/Maximum Subarray.java
@@ -0,0 +1,12 @@
+// https://leetcode.com/problems/maximum-subarray/
+
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int currsum=nums[0], maxsum=nums[0];
+        for(int i=1;i<nums.length;i++){
+            currsum=Math.max(nums[i],currsum+nums[i]);
+            maxsum=Math.max(maxsum,currsum);
+        }
+        return maxsum;
+    }
+}
