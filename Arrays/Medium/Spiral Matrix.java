4ab9a002e82b3867e82f50c272959b4fc181aeb6
Gracy Gulati <124993594+gracygulati7@users.noreply.github.com>
2025-05-07 12:39:34 +0530

    Array Q11

--- Arrays/Medium/Spiral Matrix.java
+++ Arrays/Medium/Spiral Matrix.java
@@ -5,24 +5,24 @@ class Solution {
         List<Integer> res=new ArrayList<>();
         int left=0, right=matrix[0].length-1, top=0, bottom=matrix.length-1;
         while(left<=right && top<=bottom){
-          // top
+          // Top
             for(int i=left;i<=right;i++){
                 res.add(matrix[top][i]);
             }
             top++;
-          // right
+          // Right
             for(int i=top;i<=bottom;i++){
                 res.add(matrix[i][right]);
             }
             right--;
-          // bottom
+          // Bottom
             if(top<=bottom){
                 for(int i=right;i>=left;i--){
                     res.add(matrix[bottom][i]);
                 }
                 bottom--;
             }
-          // left
+          // Left
             if(left<=right){
                 for(int i=bottom;i>=top;i--){
                     res.add(matrix[i][left]);
