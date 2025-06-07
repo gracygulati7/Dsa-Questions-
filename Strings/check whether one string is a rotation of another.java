5f10cde4a95941cb384ad3cc0785da41d37097a1
Gracy Gulati <124993594+gracygulati7@users.noreply.github.com>
2025-06-07 16:07:28 +0530

    St

---
+++ Strings/check whether one string is a rotation of another.java
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/rotate-string/
+
+class Solution {
+    public boolean rotateString(String s, String goal) {
+        if(s.length()!=goal.length()) return false;
+        String res=s+s;
+        if(res.contains(goal)) return true;
+        else return false;
+    }
+}
